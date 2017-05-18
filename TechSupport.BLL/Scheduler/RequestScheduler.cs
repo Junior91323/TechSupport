4163a2b2a2e8ddfcd3859f080cc65f9020653e22
Junior91323 <eugeneshabel@gmail.com>
2017-05-18 11:06:51 +0300

    Optimize code

--- TechSupport.BLL/Scheduler/RequestScheduler.cs
+++ TechSupport.BLL/Scheduler/RequestScheduler.cs
@@ -35,63 +35,70 @@ namespace TechSupport.BLL.Scheduler
                         List<Employee> employee = DB.Employees.GetAll().Where(x => x.RequestId == null).ToList();
                         if (employee != null && employee.Count > 0)
                         {
-                            List<Employee> operators = employee.Where(x => x.PositionId == (Int32)EmployeePositions.Operator).ToList();
-                            foreach (Request request in requests)
-                            {
-                                Employee _operator = operators.FirstOrDefault();
-                                if (_operator != null)
-                                {
-                                    _operator.RequestId = request.Id;
-                                    DB.Employees.Update(_operator);
-                                    operators.Remove(_operator);
+                            RequestToEmployee(employee, requests, EmployeePositions.Operator, 0);
+                            RequestToEmployee(employee, requests, EmployeePositions.Manager, 30);
+                            RequestToEmployee(employee, requests, EmployeePositions.Director, 60);
 
-                                    request.StateId = (Int32)RequestStates.Processing;
-                                    DB.Requests.Update(request);
-                                    requests.Remove(request);
-                                }
-                                else { break; }
-                            }
+                            #region test
 
-                            List<Request> requests2 = requests.Where(x => (DateTime.Now - x.CreatedDate).Seconds > 30).ToList();
-                            if (requests2.Count > 0)
-                            {
-                                List<Employee> managers = employee.Where(x => x.PositionId == (Int32)EmployeePositions.Manager).ToList();
-                                foreach (Request request in requests2)
-                                {
-                                    Employee manager = managers.FirstOrDefault();
-                                    if (manager != null)
-                                    {
-                                        manager.RequestId = request.Id;
-                                        DB.Employees.Update(manager);
-                                        operators.Remove(manager);
+                            //List<Employee> operators = employee.Where(x => x.PositionId == (Int32)EmployeePositions.Operator).ToList();
+                            //foreach (Request request in requests)
+                            //{
+                            //    Employee _operator = operators.FirstOrDefault();
+                            //    if (_operator != null)
+                            //    {
+                            //        _operator.RequestId = request.Id;
+                            //        DB.Employees.Update(_operator);
+                            //        operators.Remove(_operator);
 
-                                        request.StateId = (Int32)RequestStates.Processing;
-                                        DB.Requests.Update(request);
-                                        requests.Remove(request);
-                                    }
-                                    else { break; }
-                                }
-                            }
-                            List<Request> requests3 = requests.Where(x => (DateTime.Now - x.CreatedDate).Seconds > 60).ToList();
-                            if (requests3.Count > 0)
-                            {
-                                List<Employee> directors = employee.Where(x => x.PositionId == (Int32)EmployeePositions.Director).ToList();
-                                foreach (Request request in requests3)
-                                {
-                                    Employee director = directors.FirstOrDefault();
-                                    if (director != null)
-                                    {
-                                        director.RequestId = request.Id;
-                                        DB.Employees.Update(director);
-                                        operators.Remove(director);
+                            //        request.StateId = (Int32)RequestStates.Processing;
+                            //        DB.Requests.Update(request);
+                            //        requests.Remove(request);
+                            //    }
+                            //    else { break; }
+                            //}
 
-                                        request.StateId = (Int32)RequestStates.Processing;
-                                        DB.Requests.Update(request);
-                                        requests.Remove(request);
-                                    }
-                                    else { break; }
-                                }
-                            }
+                            //List<Request> requests2 = requests.Where(x => (DateTime.Now - x.CreatedDate).Seconds > 30).ToList();
+                            //if (requests2.Count > 0)
+                            //{
+                            //    List<Employee> managers = employee.Where(x => x.PositionId == (Int32)EmployeePositions.Manager).ToList();
+                            //    foreach (Request request in requests2)
+                            //    {
+                            //        Employee manager = managers.FirstOrDefault();
+                            //        if (manager != null)
+                            //        {
+                            //            manager.RequestId = request.Id;
+                            //            DB.Employees.Update(manager);
+                            //            operators.Remove(manager);
+
+                            //            request.StateId = (Int32)RequestStates.Processing;
+                            //            DB.Requests.Update(request);
+                            //            requests.Remove(request);
+                            //        }
+                            //        else { break; }
+                            //    }
+                            //}
+                            //List<Request> requests3 = requests.Where(x => (DateTime.Now - x.CreatedDate).Seconds > 60).ToList();
+                            //if (requests3.Count > 0)
+                            //{
+                            //    List<Employee> directors = employee.Where(x => x.PositionId == (Int32)EmployeePositions.Director).ToList();
+                            //    foreach (Request request in requests3)
+                            //    {
+                            //        Employee director = directors.FirstOrDefault();
+                            //        if (director != null)
+                            //        {
+                            //            director.RequestId = request.Id;
+                            //            DB.Employees.Update(director);
+                            //            operators.Remove(director);
+
+                            //            request.StateId = (Int32)RequestStates.Processing;
+                            //            DB.Requests.Update(request);
+                            //            requests.Remove(request);
+                            //        }
+                            //        else { break; }
+                            //    }
+                            //}
+                            #endregion
 
                             DB.Save();
                         }
@@ -99,6 +106,37 @@ namespace TechSupport.BLL.Scheduler
                 }
             }
             catch (Exception ex) { }
+        }
+
+        void RequestToEmployee(List<Employee> employee, List<Request> requests, EmployeePositions position, int waitingTime)
+        {
+            List<Request> subRequests = new List<Request>();
+
+            if (requests.Count > 0 && waitingTime > 0)
+                subRequests = requests.Where(x => (DateTime.Now - x.CreatedDate).Seconds > waitingTime).ToList();
+
+            if (subRequests.Count > 0)
+            {
+                List<Employee> employees = employee.Where(x => x.PositionId == (Int32)position).ToList();
+                if (employees != null && employees.Count > 0)
+                {
+                    foreach (Request request in subRequests)
+                    {
+                        Employee _employee = employees.FirstOrDefault();
+                        if (_employee != null)
+                        {
+                            _employee.RequestId = request.Id;
+                            DB.Employees.Update(_employee);
+                            employees.Remove(_employee);
+
+                            request.StateId = (Int32)RequestStates.Processing;
+                            DB.Requests.Update(request);
+                            requests.Remove(request);
+                        }
+                        else { break; }
+                    }
+                }
+            }
         }
     }
 }
