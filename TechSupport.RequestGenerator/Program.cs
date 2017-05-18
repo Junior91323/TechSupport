0d546cb933302135d550f3464f06d93e5e20c6c9
Junior91323 <eugeneshabel@gmail.com>
2017-05-18 11:11:29 +0300

    Add console app which send requests on TechSupport API

---
+++ TechSupport.RequestGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.RequestGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
